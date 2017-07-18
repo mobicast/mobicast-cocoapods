9e853293c95bc4f5554d0fe9fd847a3060ca7622
Gunars Grundstoks <gunars@ithouse.io>
2017-07-18 19:09:38 +0300

    Update Splash screen design

--- Example/Mobicast/ListViewController.swift
+++ Example/Mobicast/ListViewController.swift
@@ -14,6 +14,8 @@ class ListViewController: UIViewControllerExtension, UITableViewDelegate, UITabl
 
         // Do any additional setup after loading the view.
 
+        UIApplication.shared.setStatusBarHidden(false, with: UIStatusBarAnimation.fade)
+
         setupTableViewData()
     }
 
